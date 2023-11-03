ebacdae1200952443b12511dbe92d72c064d47d9
JaeWon Jin <j1jin286@gmail.com>
2023-11-03 10:59:05 -0700

    Added some testing for TodoApp

--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, within } from "@testing-library/react";
 import TodoApp from "./TodoApp.js";
 import TEST_TODOS from "./_testCommon.js";
 
-describe("smoketest", function () {
+describe("TodoApp", function () {
   it("renders without crashing", function () {
     render(<TodoApp initialTodos={TEST_TODOS} />);
   });
@@ -19,6 +19,35 @@ describe("smoketest", function () {
 
     expect(queryByText("Top Todo")).toBeNull();
   });
+
+  it("edit todo and saving updates todo list", function () {
+    const { container, getByText } = render(<TodoApp initialTodos={[TEST_TODOS[0]]} />);
+
+    const editableTodo = container.querySelector(".EditableTodo");
+
+    expect(within(editableTodo).getByText("test1title")).toBeInTheDocument();
+    expect(within(editableTodo).getByText("test1description")).toBeInTheDocument();
+    expect(within(editableTodo).getByText("(priority: 1)")).toBeInTheDocument();
+
+    const editButton = getByText("Edit");
+    fireEvent.click(editButton);
+
+    expect(editableTodo.querySelector("#newTodo-title")).toBeInTheDocument();
+    expect(editableTodo.querySelector("#newTodo-description")).toBeInTheDocument();
+    expect(editableTodo.querySelector("#newTodo-priority")).toBeInTheDocument();
+
+    // const saveButton = getByText("Gø!");
+    // fireEvent.click(saveButton);
+
+    // expect(container.querySelector("#newTodo-title")).not.toBeInTheDocument();
+    // expect(container.querySelector("#newTodo-description")).not.toBeInTheDocument();
+    // expect(container.querySelector("#newTodo-priority")).not.toBeInTheDocument();
+  });
+  //Edit button saves changes/update
+  //Priority changes
+  //Add new todo render/ Creating todo adds to list
+  //Delete todo
+
 
   // it("displays all todos", function () {
   //   const { getByText } = render(<EditableTodoList todos={TEST_TODOS} />);
